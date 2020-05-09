2ccf7c6801b2c5719d954d782648b2278ad59e67
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-05-09 16:39:08 +0100

    added phone images

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@
 
 (function() {
 	var alpha = beta = gamma = 0;
+
 	
+	var messageContainer = document.getElementById('messageContainer');
+
 	if (window.DeviceOrientationEvent){
 		window.addEventListener('deviceorientation', update, true);
 	
@@ -19,18 +22,20 @@
 	function update(event){
 		var debug = document.getElementById('debug');
 		
+		var transform = messageContainer.style.transform;
+		
 		alpha = Math.floor(event.alpha);
 		beta = Math.floor(event.beta);
 		gamma = Math.floor(event.gamma);
 		
-		debug.textContent = 'Alpha: '+alpha+ ', Beta: ' +beta+ ', and Gamma is: ' +gamma;
+		debug.textContent = 'Alpha: '+alpha+ ', Beta: ' +beta+ ', and Gamma is: ' +gamma;		
 		
-		
-		// a device is place on a flat surface
-				
-		message.textContent = answer;
-		
+		transform = 'rotateZ(' +alpha+ 'deg) rotateX(' +beta+ 'deg) rotateY(' +gamma+ 'deg)';			
+		// 3D Transform	
+		messageContainer.style.webkitTransform = messageContainer.style.MozTransform = messageContainer.style.transform = transform;
+		// 2D-only for Opera
+		messageContainer.style.OTransform = 'rotate(' +alpha+ 'deg)'; 
+
 	}
 
 })();
-
