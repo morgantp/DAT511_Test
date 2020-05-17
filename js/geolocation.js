f2d3d27e501e97330a9bed2cd95e3b5353f146c6
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-05-17 17:21:17 +0100

    added geolocation

---
+++ js/geolocation.js
@@ -0,0 +1,17 @@
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+        var map = L.map('map').setView([latitude, longitude], 12);
+
+        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=sJdX2R69yUO7n4qEW4gl', {
+            attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>' ,
+        }).addTo(map);
+        console.log(latitude, longitude);
+}, error => {
+    console.log(error.code);
+},{
+    enableHighAccuracy: true
+});
+}
