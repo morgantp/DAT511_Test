dee490233555535c2e4d6b0050f5045843006bd1
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-05-21 19:58:49 +0100

    added working compass... hopefully

--- Morgsjs/compass.js
+++ Morgsjs/compass.js
@@ -1,23 +1,7 @@
-(function() {
+var compass = document.getElementById('compass');
+var info = document.getElementById('compassInfo');
 
-	var compassContainer = document.getElementById('compassContainer');
-
-	navigator.geolocation.watchPosition(position => {
-        let heading = position.coords.heading;
-        window.addEventListener('heading', update, true);
-
-	
-        function update(event){
-            var compass = document.getElementById('compass');
-            
-            var transform = compassContainer.style.transform;
-            
-            compass.textContent = 'Heading: '+heading;		
-            
-            compassContainer.style.webkitTransform = compassContainer.style.MozTransform = compassContainer.style.transform = transform;
-            // 2D-only for Opera
-            compassContainer.style.OTransform = 'rotate(' +heading+ 'deg)'; 
-        }
-})
-
-})();
+navigator.geolocation.watchPosition((data) => {
+    info.textContent = 'Heading: '+data.coords.heading;
+    compass.style.transform = `rotate(${data.coords.heading}deg)`
+});
