3ab88474bd2a8b8d591382f2f6d281216aaa60ea
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-05-21 19:03:32 +0100

    added compass rotation

--- Morgsjs/compass.js
+++ Morgsjs/compass.js
@@ -2,6 +2,15 @@ navigator.geolocation.watchPosition(position => {
     let heading = position.coords.heading;
 
     var compass = document.getElementById('compass');
+    var compassContainer = document.getElementById('compassContainer')
 
     compass.textContent = 'Heading: ' +heading;
+
+
+    var transform = compassContainer.style.transform;
+		    
+    
+    transform = 'rotate(' +heading;
+    compassContainer.style.webkitTransform = compassContainer.style.MozTransform = compassContainer.style.transform = transform;
+		
 });
