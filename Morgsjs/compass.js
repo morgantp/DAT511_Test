4de27fd06dae8565970ccc4892028ee505cb1cb0
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-05-21 19:36:00 +0100

    added compass rotation2

--- Morgsjs/compass.js
+++ Morgsjs/compass.js
@@ -1,16 +1,23 @@
-navigator.geolocation.watchPosition(position => {
-    let heading = position.coords.heading;
+(function() {
 
-    var compass = document.getElementById('compass');
-    var compassContainer = document.getElementById('compassContainer')
+	var compassContainer = document.getElementById('compassContainer');
 
-    compass.textContent = 'Heading: ' +heading;
+	navigator.geolocation.watchPosition(position => {
+        let heading = position.coords.heading;
+        window.addEventListener('heading', update, true);
 
+	
+        function update(event){
+            var compass = document.getElementById('compass');
+            
+            var transform = compassContainer.style.transform;
+            
+            compass.textContent = 'Heading: '+heading;		
+            
+            compassContainer.style.webkitTransform = compassContainer.style.MozTransform = compassContainer.style.transform = transform;
+            // 2D-only for Opera
+            compassContainer.style.OTransform = 'rotate(' +heading+ 'deg)'; 
+        }
+})
 
-    var transform = compassContainer.style.transform;
-		    
-    
-    transform = 'rotateZ(' +heading;
-    compassContainer.style.webkitTransform = compassContainer.style.MozTransform = compassContainer.style.transform = transform;
-		
-});
+})();
